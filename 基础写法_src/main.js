4f12d7b6fd173ee6fe4fdbb7afdbf4b1bb271d3a
ballzing <1048315549@qq.com>
2022-10-11 11:25:23 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Count.vue 	modified:   src/main.js 	new file:   "\345\237\272\347\241\200\345\206\231\346\263\225_src/App.vue" 	new file:   "\345\237\272\347\241\200\345\206\231\346\263\225_src/assets/logo.png" 	new file:   "\345\237\272\347\241\200\345\206\231\346\263\225_src/components/Count.vue" 	new file:   "\345\237\272\347\241\200\345\206\231\346\263\225_src/main.js"

--- 基础写法_src/main.js
+++ 基础写法_src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
+
 Vue.config.productionTip = false
-Vue.use(Vuex)
 
 new Vue({
   render: h => h(App),
-  store: 'hello',
-  beforeCreate() {
-    Vue.prototype.$bus = this;
-  }
 }).$mount('#app')
